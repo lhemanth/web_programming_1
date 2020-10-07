fe28d89bd11846092a4323a1bc3e16dbf79c6be7
lhemanth <hlella@kent.edu>
2020-10-06 22:25:23 -0400

    web home work 1

--- bottle_app.py
+++ bottle_app.py
@@ -1,8 +1,8 @@
 # A very simple Bottle Hello World app for you to get started with...
 import os
+import sqlite3
 
-
-from bottle import  template, route
+from bottle import  get, post, request, template, redirect, route
 
 ON_PYTHONANYWHERE = "PYTHONANYWHERE_DOMAIN" in os.environ.keys()
 
@@ -11,14 +11,19 @@ if ON_PYTHONANYWHERE:
 else:
     from bottle import run, debug
 
-@route('/hello/<name>')
+@route('/<name>')
 def index(name):
-    return template('<b>Hello {{name}}</b>!', name=name)
+    return template('<h1>Hello {{name}} Welcome to Web Programming 1!</h1>', name=name)
 
 
 @route('/')
-def test():
-    return template("index")
+def get_show_list():
+    connection = sqlite3.connect("work.db")
+    cursor = connection.cursor()
+    cursor.execute("select * from todo")
+    result = cursor.fetchall()
+    cursor.close()
+    return template("index", rows=result)
 
 
 if ON_PYTHONANYWHERE:
--- index.tpl
+++ index.tpl
@@ -1,5 +1,28 @@
 <html>
+<head>
+<title>Web Programming</title>
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"/>
+<link href="https://www.w3schools.com/w3css/4/w3.css" rel="stylesheet" >
+</head>
 <body>
-<h1> Hello Welcome to Web Programming 1! </h1>
+<h3 class=" w3-center"><b>Web Programming Home Work 1</b></h3>
+
+
+<table class="w3-table w3-bordered w3-border">
+<tr>
+<td><b>ID</b></td>
+<td><b> Name </b> </td>
+<td><b>Status </b></td>
+</tr>
+
+%for row in rows: 
+    <tr>
+    %for i in row:
+        <td>{{str(i)}}</td>
+    %end
+    </tr>
+%end
+</table>
+
 </body>
 </html>
